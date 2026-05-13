b71122523ad103c1abe1c12638bf911db18e7ca7
Qandor <109076431+HackerZsalmale@users.noreply.github.com>
2026-05-13 19:15:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,45 @@
 const lenis = new Lenis();
 
-  function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-  }
-
+function raf(time) {
+  lenis.raf(time);
   requestAnimationFrame(raf);
+}
 
+requestAnimationFrame(raf);
 
-
-
-
-
-
-window.addEventListener("load", function() {
-    loadContent();
+window.addEventListener("load", function () {
+  loadContent();
 });
 
 async function loadContent() {
+  const homesection = 1;
+  const aboutsection = 0;
+  const skillssection = 0;
 
-const homesection = 1;
-const aboutsection = 0;
-const skillssection = 0;
-
-if (homesection === 1) {
+  if (homesection === 1) {
     document.getElementById("home-section").style.display = "flex";
-} else {
+  } else {
     document.getElementById("home-section").style.display = "none";
-};
+  }
 
-if (aboutsection === 1) {
+  if (aboutsection === 1) {
     document.getElementById("about-section").style.display = "flex";
-} else {
+  } else {
     document.getElementById("about-section").style.display = "none";
-};
+  }
 
-if (skillssection === 1) {
+  if (skillssection === 1) {
     return;
-} else {
+  } else {
     document.getElementById("skills-section").style.display = "none";
-};
+  }
 
-if (homesection === 1 && aboutsection === 0 && skillssection === 0) {
-  document.body.style.height = "92vh";
+  if (homesection === 1 && aboutsection === 0 && skillssection === 0) {
+    document.body.style.height = "92vh";
     document.getElementById("home-section").style.height = "92vh";
-};
-
+  }
 }
 
-document.getElementById("github").addEventListener("click", function() {
-    window.open("https://github.qandor.hu", "_self");
+document.getElementById("github").addEventListener("click", function () {
+  window.open("https://github.qandor.hu", "_self");
 });
-
-
