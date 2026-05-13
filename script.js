dd82b6c1e08df1b57ba41e7d3b43a50f1bbf2deb
Qandor <109076431+HackerZsalmale@users.noreply.github.com>
2026-05-13 19:12:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,48 @@ const lenis = new Lenis();
   requestAnimationFrame(raf);
 
 
-document.getElementById("home").addEventListener("click", function() {
-    lenis.scrollTo(0, { offset: 1000 });
-});
 
-BeforeUnloadEvent.addEventListener("beforeunload", function() {
-    localStorage.setItem("scrollPosition", window.scrollY);
-});
+
+
+
 
 window.addEventListener("load", function() {
-    const scrollPosition = localStorage.getItem("scrollPosition");
-    scrollTo(0, scrollPosition);
+    loadContent();
 });
+
+async function loadContent() {
+
+const homesection = 1;
+const aboutsection = 0;
+const skillssection = 0;
+
+if (homesection === 1) {
+    document.getElementById("home-section").style.display = "flex";
+} else {
+    document.getElementById("home-section").style.display = "none";
+};
+
+if (aboutsection === 1) {
+    document.getElementById("about-section").style.display = "flex";
+} else {
+    document.getElementById("about-section").style.display = "none";
+};
+
+if (skillssection === 1) {
+    return;
+} else {
+    document.getElementById("skills-section").style.display = "none";
+};
+
+if (homesection === 1 && aboutsection === 0 && skillssection === 0) {
+  document.body.style.height = "92vh";
+    document.getElementById("home-section").style.height = "92vh";
+};
+
+}
+
+document.getElementById("github").addEventListener("click", function() {
+    window.open("https://github.qandor.hu", "_self");
+});
+
+
