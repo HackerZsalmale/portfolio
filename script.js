a5db16d671363c0ad5111c56fe354fd305275046
Qandor <109076431+HackerZsalmale@users.noreply.github.com>
2026-05-21 16:21:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,38 +7,9 @@ function raf(time) {
 
 requestAnimationFrame(raf);
 
-window.addEventListener("load", function () {
-  loadContent();
-});
 
-async function loadContent() {
-  const homesection = 1;
-  const aboutsection = 0;
-  const skillssection = 0;
 
-  if (homesection === 1) {
-    document.getElementById("home-section").style.display = "flex";
-  } else {
-    document.getElementById("home-section").style.display = "none";
-  }
 
-  if (aboutsection === 1) {
-    document.getElementById("about-section").style.display = "flex";
-  } else {
-    document.getElementById("about-section").style.display = "none";
-  }
-
-  if (skillssection === 1) {
-    return;
-  } else {
-    document.getElementById("skills-section").style.display = "none";
-  }
-
-  if (homesection === 1 && aboutsection === 0 && skillssection === 0) {
-    document.body.style.height = "92vh";
-    document.getElementById("home-section").style.height = "92vh";
-  }
-}
 
 async function github() {
     window.open("https://github.qandor.hu", "_self");
