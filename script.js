853fecc419124f0af3db7b80d998007aac996e5b
Qandor <109076431+HackerZsalmale@users.noreply.github.com>
2026-05-28 15:15:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,52 @@ function raf(time) {
 
 requestAnimationFrame(raf);
 
+window.addEventListener("load", function () {
+  loadContent();
+});
+
+function home() {
+  document
+    .getElementById("homebutton")
+    .addEventListener("click", function () {
+      lenis.scrollTo("#home-section", {
+        offset: 0,
+        immediate: false,
+        duration: 1.5,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        force: true,
+      });
+    });
+}
+
+function about() {
+  document
+    .getElementById("aboutbutton")
+    .addEventListener("click", function () {
+      lenis.scrollTo("#about-section", {
+        offset: 0,
+        immediate: false,
+        duration: 1.5,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        force: true,
+      });
+    });
+}
+
+function skills() {
+  document
+    .getElementById("skillsbutton")
+    .addEventListener("click", function () {
+      lenis.scrollTo("#skills-section", {
+        offset: 0,
+        immediate: false,
+        duration: 1.5,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        force: true,
+      });
+    });
+}
 
 async function github() {
-    window.open("https://github.qandor.hu", "_self");
+  window.open("https://github.qandor.hu", "_self");
 }
